37dbf5cad81320ec825cb1ae42e26c4ebcd87f20
janethe77 <jtedjajuwana@my.harrisburgu.edu>
2017-05-31 16:29:41 -0400

    First Clean Up
    Removed column Strange HTML, cleared trailing spaces, removed special
    char, and fixed a few misspellings

---
+++ first_edit.R
@@ -0,0 +1,22 @@
+dirty_data_1<- read.csv("dirty_data.csv",header=TRUE,strip.white = TRUE)
+summary(dirty_data_1)
+library(tidyr)
+library(dplyr)
+str(dirty_data_1)
+#Replacing all special characters with a space
+dirty_data_1$Street<-gsub('[^a-zA-Z0-9]',' ',dirty_data_1$Street)
+
+#Replace trailing spaces
+trim <- function( x ) {
+  gsub("(^[[:space:]]+|[[:space:]]+$)", "", x)
+}
+dirty_data_1$Street<-trim(dirty_data_1$Street)
+
+#Remove Strange HTML Column
+dirty_data_1<-dirty_data_1[,-5]
+
+#Replace word in a text to correct misspellings
+dirty_data_1$Street<-gsub("[sS]treet","Str.",dirty_data_1$Street)
+dirty_data_1$Street.2<-gsub("[sS]treet","Str.",dirty_data_1$Street.2)
+dirty_data_1$Street<-gsub("[rR][oa][ao]d","Road",dirty_data_1$Street)
+dirty_data_1$Street.2<-gsub("[rR][oa][ao]d","Road",dirty_data_1$Street.2)
